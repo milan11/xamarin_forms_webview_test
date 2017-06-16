a7a0038efd8ac153378f290a3b21a67690869c8e
Milan Laslop <milan33@gmail.com>
2017-06-16 10:12:22 +0200

    initial

---
+++ App6/App6/BrowserPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App6
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BrowserPage : ContentPage
+    {
+        public BrowserPage()
+        {
+            InitializeComponent();
+
+            StringBuilder text = new StringBuilder();
+            for (int i = 0; i < 100000; ++i)
+            {
+                text.Append("hello ");
+            }
+
+            WebView1.Source = new HtmlWebViewSource() { Html = text.ToString() };
+        }
+    }
+}
---
+++ App6/App6/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace App6
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new BrowserPage());
+        }
+    }
+}
